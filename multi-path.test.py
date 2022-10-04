f52fd76db8ac81fa6122b318f8cee55814834ce7
Matthew Ruge <matthew.ruge@shrgroupllc.com>
2022-10-04 19:58:14 +0000

    analyzes multiple options

--- multi-path.test.py
+++ multi-path.test.py
@@ -1,4 +1,5 @@
 import uuid, jinja2
+
 class Edge:
     def __init__(self, a, b, cost=0, operation='none'):
         self.a = a
@@ -10,9 +11,12 @@ class Edge:
         return f"{self.a.name} -> {self.b.name} : {self.cost}"
 
 class Node:
-    def __init__(self, name=""):
+    def __init__(self, name="", id=None):
         self.name = name
-        self.id = str(uuid.uuid4().hex)
+        if not id:
+            self.id = str(uuid.uuid4().hex)
+        else:
+            self.id = id
     def __eq__(self, other):
         return self.id == other.id
     def __str__(self):
@@ -31,9 +35,18 @@ class Path:
     def __str__(self):
         return '->'.join([str(e.b) for e in self.edges])+f": {self.cost}"
 
+    def __iter__(self):
+        node_list = []
+        [node_list.append(n) for e in self.edges for n in [e.a, e.b] if n not in node_list ]
+        for n in node_list: yield n
+
     @property
     def head(self):
         return self.edges[0].a
+
+    @property
+    def option(self):
+        return self.edges[0].b
 
     @property
     def tail(self):
@@ -49,13 +62,56 @@ class Path:
     def endswith(self, node):
         return self.tail == node
 
-    def not_contains(self, node):
-        return node not in self.nodes
+    # def contains(self, node):
+    #     return node in self.nodes
+
+    # def not_contains(self, node):
+    #     return node not in self.nodes
+
+class Flow:
+    def __init__(self, paths):
+        self.paths = paths
+
+    def split(self, n1, n2):
+        return (
+            Flow([p for p in self.paths if n1 not in p]),
+            Flow([p for p in self.paths if n2 not in p])
+        )
+
+    def __str__(self):
+        return ','.join(self.options)
+        return str(len(f"paths: {len(self.paths)}"))
+        return '\n'.join([str(p) for p in self.paths])
+    
+    @property
+    def cost(self):
+        return sum([p.cost for p in self.paths])
+
+    @property
+    def options(self):
+        uniq = []
+        return set([p.option.name for p in self.paths])
+
+    @property
+    def count(self):
+        return len(self.paths)
 
 def get_node(id):
     for node in nodes:
         if node.id == id:
             return node
+
+# def filter(paths, constraint, invert=False):
+
+
+# def all_flows(all_paths, constraints):
+#     flows = []
+    
+#     for c1 in constraints:
+#         paths_with = filter(paths, c1)
+#         paths_without = filter(paths, c1, invert=True)
+#         for c2 in [c for c in constraints if c not c1]:
+#             flows += Flow(filtered)
 
 def all_paths(goal, paths):
     if isinstance(paths, list):
@@ -67,46 +123,8 @@ def all_paths(goal, paths):
     else:
         return all_paths(goal,[Path(e) for e in edges if e.a == paths])
 
-def filter(paths):
-    groups = []
-    constraints = [e for e in edges if e.op != 'none']
-    for c in constraints:
-        if c.op == 'OR':
-            groups += [p for p in paths if p.contains(c.a) and p.not_contains(c.b)]
-            groups += [p for p in paths if p.not_contains(c.a) and p.contains(c.b)]
-
-
-
-# car = Node('car')
-# lx = Node('lx')
-# ls = Node('ls')
-# features = Node('Features')
-# cost = Node('cost')
-# goal = Node('goal')
-# base = Node('basecost')
-
-# e1 = Edge(car, lx)
-# e2 = Edge(car, ls)
-# c1 = Edge(lx, ls, operation='or')
-
-# e3 = Edge(ls, cost, -1)
-# e4 = Edge(lx, cost, -2)
-# e5 = Edge(ls, features, 5)
-# e6 = Edge(lx, features, 9)
-
-# e7 = Edge(cost, goal, 0)
-# e8 = Edge(features, goal, 0)
-
-# e10 = Edge(car, base)
-# e11 = Edge(base, cost, -10)
-# import gc
-# nodes = [obj for obj in gc.get_objects() if isinstance(obj, Node)]
-# edges = [obj for obj in gc.get_objects() if isinstance(obj, Edge)]
-
-
-# paths = all_paths(goal, car)
-# groups = filter(paths)
-
+def paths_contain(paths, node):
+    return [p for p in paths if p.contains(node)]
 
 
 mac = Node('Mac')
@@ -116,42 +134,97 @@ s3 = Node('4c')
 s4 = Node('8c')
 s5 = Node('$2k')
 
-
 mem = Node('mem')
 cpu = Node('cpu')
 cost = Node('cost')
 
-
 group_cost = Node('cost')
 perf = Node('performance')
-
-edges = []
-edges += [Edge(mac, s1)]
-edges += [Edge(mac, s2)]
-edges += [Edge(mac, s3)]
-edges += [Edge(mac, s4)]
-edges += [Edge(mac, s5)]
-edges += [Edge(s1, mem, 8)]
-edges += [Edge(s2, mem, 16)]
-edges += [Edge(s3, cpu, 4)]
-edges += [Edge(s4, cpu, 8)]
-
-edges += [Edge(s1, cost, 1)]
-edges += [Edge(s2, cost, 8)]
-edges += [Edge(s3, cost, 1)]
-edges += [Edge(s4, cost, 2)]
-edges += [Edge(s5, cost, 200)]
-
-edges += [Edge(mem, perf, 1)]
-edges += [Edge(cpu, perf, 1)]
-edges += [Edge(cost, group_cost, 1)]
 goal = Node('goal')
-edges += [Edge(group_cost, goal)]
-edges += [Edge(perf, goal)]
+edges = [
+    Edge(mac, s1),
+    Edge(mac, s2),
+    Edge(mac, s3),
+    Edge(mac, s4),
+    Edge(mac, s5),
+    Edge(s1, mem, 8),
+    Edge(s2, mem, 16),
+    Edge(s3, cpu, 4),
+    Edge(s4, cpu, 8),
 
+    Edge(s1, cost, 1),
+    Edge(s2, cost, 8),
+    Edge(s3, cost, 1),
+    Edge(s4, cost, 2),
+    Edge(s5, cost, 200),
 
+    Edge(mem, perf, 1),
+    Edge(cpu, perf, 1),
+    Edge(cost, group_cost, 1),
+
+    Edge(group_cost, goal),
+    Edge(perf, goal),
+]
 import gc
 nodes = [obj for obj in gc.get_objects() if isinstance(obj, Node)]
+constraints = [
+    (s1, s2),
+    (s3, s4)
+]
+
+paths = all_paths(nodes[9],nodes[0])
+flows = [Flow(paths)]
+
+for c in constraints:
+    new_flows = []
+    for i in range(len(flows)):
+        f = flows.pop()
+        f1, f2 = f.split(*c)
+        # print(f"{f} => {f1},{f2}")
+        new_flows.append(f1)
+        new_flows.append(f2)
+    flows = new_flows
+
+
+for flow in flows:
+    print(flow, flow.cost)
+    print("---")
+
+
+### CONSTRAINT TEST ###
+# nodes = [Node(i) for i in range(10)]
+# edges = [
+#     Edge(nodes[0],nodes[1],1),
+#     Edge(nodes[0],nodes[2],1),
+#     Edge(nodes[0],nodes[3],1),
+#     Edge(nodes[0],nodes[4],1),
+#     Edge(nodes[1],nodes[5],1),
+#     Edge(nodes[2],nodes[6],1),
+#     Edge(nodes[3],nodes[7],1),
+#     Edge(nodes[4],nodes[8],1),
+#     Edge(nodes[5],nodes[9],1),
+#     Edge(nodes[6],nodes[9],1),
+#     Edge(nodes[7],nodes[9],1),
+#     Edge(nodes[8],nodes[9],1),
+# ]
+# constraints = [
+#     (nodes[1], nodes[2]),
+#     (nodes[3], nodes[4])
+# ]
+
+# paths = all_paths(nodes[9],nodes[0])
+# flows = [Flow(paths)]
+
+# for c in constraints:
+#     for i in range(len(flows)):
+#         f = flows.pop()
+#         f1, f2 = f.split(*c)
+#         flows.append(f1)
+#         flows.append(f2)
+
+
+
+
 # edges = [obj for obj in gc.get_objects() if isinstance(obj, Edge)]
 j2 = """
 digraph {
@@ -169,4 +242,5 @@ digraph {
 }"""
 with open('testgraph.gv', 'w') as f:
     f.write(jinja2.Template(j2).render(nodes=nodes, edges=edges))
+
 
